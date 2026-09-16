d2eacf39a6a5d3cd9d3e0bba8c21279959ee63ef
joaozinxd0-cyber <joaozinxd0@gmail.com>
2026-09-16 08:12:47 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const botao = document.getElementById('botaoTema');
+
+botao.addEventListener('click', () => {
+document.body.classList.toggle('tema-escuro');
+
+const estaEscuro = document.body.classList.contains('tema-escuro');
+
+botao.textContent = estaEscuro
+? 'Ativar tema claro'
+: 'Ativar tema escuro';
+});
